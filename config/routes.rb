3fa381a9fc1c846453e2549756fc2826580479d0
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-05-23 07:41:38 +0100

    Setup devise for local authentication of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :guests
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
