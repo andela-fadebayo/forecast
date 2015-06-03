b0362d6601d3804daececc4b40be891a699d12aa
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-06-03 15:29:36 +0100

    Edit and Delete Todo lists

--- app/controllers/shopquik_todo_lists_controller.rb
+++ app/controllers/shopquik_todo_lists_controller.rb
@@ -1,9 +1,9 @@
 class ShopquikTodoListsController < ApplicationController
   before_action :authenticate_guest!
-  before_action :set_todo_list, only: [:show]
+  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
   def index
-    @todo_lists = Shopquik::TodoList.all
+    @todo_lists = Shopquik::TodoList.all.order("updated_at DESC")
   end
 
   def show
@@ -28,6 +28,29 @@ class ShopquikTodoListsController < ApplicationController
       else
         format.html { render :index, alert: 'Shopping list was not created!' }
       end
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @todo_list.update(todo_list_params)
+        format.html { redirect_to shopquik_todo_lists_path, notice: 'Shopping list updated successfully.' }
+      else
+        format.html { render :index, alert: 'Failed to update shopping list!' }
+      end
+    end
+  end
+
+  def destroy
+    @todo_list.destroy
+    respond_to do |format|
+      format.html { redirect_to shopquik_todo_lists_path, notice: 'Shopping list was deleted successfully!' }
     end
   end
 
