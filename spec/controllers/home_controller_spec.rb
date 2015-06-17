7888460d4e583c240f8b4ce2e4c2d00f6e4fc791
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-06-17 23:44:03 +0100

    Fixed failing home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe HomeController, type: :controller do
   describe "GET #index" do
+    let(:guest) { create(:guest) }
+
+    before do
+      allow(request.env['warden']).to receive(:authenticate!) { guest }
+      allow(controller).to receive(:current_guest) { guest }
+    end
+
     it "renders the index template" do
       get :index
       expect(response).to render_template :index
