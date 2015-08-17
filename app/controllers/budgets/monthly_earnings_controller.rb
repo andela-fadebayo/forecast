12a5140dcd63487d1a20f9135d6f53590c4beed9
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-08-17 10:49:26 +0100

    Add new budget earning and Include contact map

--- app/controllers/budgets/monthly_earnings_controller.rb
+++ app/controllers/budgets/monthly_earnings_controller.rb
@@ -4,6 +4,8 @@ class Budgets::MonthlyEarningsController < ApplicationController
   def index
     @home_bg = 'budget-bg'
     @title = 'Budget - Monthly Earnings'
+    @monthly_earnings = current_guest.budgets_monthly_earnings.order("updated_at ASC")
+    @monthly_earning = Budgets::MonthlyEarning.new
   end
 
   def show
@@ -13,14 +15,29 @@ class Budgets::MonthlyEarningsController < ApplicationController
   end
 
   def create
+    @monthly_earning = current_guest.budgets_monthly_earnings.new(me_params)
+    respond_to do |format|
+      if @monthly_earning.save
+        format.html { redirect_to budgets_monthly_earnings_path, notice: 'New monthly earning added.' }
+      else
+        format.html { render :index, alert: 'Error adding new earning.' }
+      end
+    end
   end
 
   def edit
+    @monthly_earning = current_guest.budgets_monthly_earnings.find(params[:id].to_i)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def me_params
+    params.require(:budgets_monthly_earning).permit(:monthyear, :earning)
   end
 end
