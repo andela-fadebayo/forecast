85ecb019524a65ec597b73c8b4e81c3551540028
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-08-17 10:53:50 +0100

    Remove #new block in monthly_earnings_controller_spec

--- spec/controllers/budgets/monthly_earnings_controller_spec.rb
+++ spec/controllers/budgets/monthly_earnings_controller_spec.rb
@@ -22,13 +22,6 @@ RSpec.describe Budgets::MonthlyEarningsController, type: :controller do
   #   end
   # end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   # describe "GET #create" do
   #   it "returns http success" do
   #     get :create
