e45580cae8e1b8ee99a4247b21ead51a8e3c6d9d
andela-oagboola <olaide.agboola@andela.co>
2015-09-21 17:15:38 +0100

    working on budgets

--- app/controllers/budgets/monthly_earnings_controller.rb
+++ app/controllers/budgets/monthly_earnings_controller.rb
@@ -27,9 +27,17 @@ class Budgets::MonthlyEarningsController < ApplicationController
 
   def edit
     @monthly_earning = current_guest.budgets_monthly_earnings.find(params[:id].to_i)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update
+    monthly_earning = current_guest.budgets_monthly_earnings.find(params[:id].to_i)
+    if monthly_earning.update_attributes(me_params)
+      redirect_to index
+    end
   end
 
   def destroy
--- spec/views/budgets/monthly_earnings/edit.html.haml_spec.rb
+++ spec/views/budgets/monthly_earnings/edit.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "monthly_earnings/edit.html.haml", type: :view do
+RSpec.describe "monthly_earnings/_edit.html.haml", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
